3927f49d955a0a8309c407472c4e6360ee13c8a4
Crunch <adam@wearecrunch.dk>
2016-04-21 17:31:38 +0200

    initial commit

---
+++ wp-content/themes/responsive/template-parts/featured-area.php
@@ -0,0 +1,23 @@
+<?php
+
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+$responsive_options = responsive_get_options();
+//test for first install no database
+$db = get_option( 'responsive_theme_options' );
+//test if all options are empty so we can display default text if they are
+$empty     = ( empty( $responsive_options['home_headline'] ) && empty( $responsive_options['home_subheadline'] ) && empty( $responsive_options['home_content_area'] ) ) ? false : true;
+$emtpy_cta = ( empty( $responsive_options['cta_text'] ) ) ? false : true;
+
+?>
+
+<div id="featured" class="grid col-940">
+
+	<?php 
+    echo do_shortcode("[metaslider id=44 percentwidth=100]"); 
+?>
+
+</div><!-- end of #featured -->
